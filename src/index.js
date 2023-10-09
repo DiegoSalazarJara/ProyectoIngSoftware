ebb076099f6e97502c9ba36a047c9744c70aed80
DiegoSalazarJara <130184364+DiegoSalazarJara@users.noreply.github.com>
2023-10-09 15:42:16 -0300

    Creacion variables de entorno y conexion mongoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, {json} from 'express';
+import { setupDB } from './config/db.config.js';
+import postulacionRoutes from './routes/postulacion.routes.js';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 5000;
+
+//middleware
+app.use(json());
+
+//ruta postulacion
+app.use("/api", postulacionRoutes);
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`API en la url http://localhost:${port}`);
+    setupDB();
+  });
