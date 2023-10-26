d9cd9afb4820394ecc13a634cd6fe14ade345a3a
DiegoSalazarJara <130184364+DiegoSalazarJara@users.noreply.github.com>
2023-10-25 23:47:23 -0300

    modificacion routes postulacion

--- src/routes/postulacion.routes.js
+++ src/routes/postulacion.routes.js
@@ -1,9 +1,39 @@
-import {Router} from "express";
-import {createForms} from '../controllers/postulacion.controller.js'
+import express from "express";
+import multer from "multer";
+import bodyParser from "body-parser";
+import { createForms, deleteForm, getForms, getFormsAll, updateForm } from '../controllers/postulacion.controller.js';
+/** Middleware de autenticación postulante */
+import authenticationMiddleware from '../middlewares/authentication.middleware.js';
+import authorizationMiddleware from "../middlewares/authorization.middleware.js";
+const router = express.Router();
+const jsonParser = bodyParser.json({ limit: '50mb' });
 
-const router = Router();
+// Configuración del almacenamiento de archivos
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './src/upload/'); 
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
 
-//crear la ruta
-router.post('/postulacion', createForms)
+const upload = multer({ storage: storage });
+
+router.use(authenticationMiddleware);
+// Crear la ruta
+router.post(
+    '/',jsonParser,upload.fields([
+      { name: 'certificadoResidencia', maxCount: 1 },
+      { name: 'certificadoConstitucion', maxCount: 1 },
+      { name: 'fotocopiaCarnet', maxCount: 1 },
+      { name: 'certificadoArriendo', maxCount: 1 }
+    ]), authorizationMiddleware.isPostulante, createForms);
+  
+
+router.get('/:rutpostulante', authorizationMiddleware.isPostulante, getForms);
+router.get('/', getFormsAll)
+router.delete('/:id', authorizationMiddleware.isPostulante, deleteForm);
+router.put('/:id', authorizationMiddleware.isPostulante, updateForm);
 
 export default router;
