7ee58081336d57d0eb8b084e1bc6c0a18c9b27f0
Israel Castillo Fernandez <pablo.castillo2101@alumnos.ubiobio.cl>
2023-10-29 17:36:09 -0300

    Se añadio el envio de correo en controller.respuesta.js

--- src/controllers/respuesta.controller.js
+++ src/controllers/respuesta.controller.js
@@ -1,6 +1,8 @@
 import Respuesta from '../models/respuesta.model.js';
 import Evaluador from '../models/evaluador.model.js';
 import Postulacion from '../models/postulacion.model.js';
+import sgMail from "@sendgrid/mail";
+
 
 export const clasificarPostulacion = async (req, res) => {
   try {
@@ -32,6 +34,45 @@ export const clasificarPostulacion = async (req, res) => {
       evaluar: evaluar,
     });
 
+
+    if (evaluar === 'aprobado') {
+      const API_KEY = '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';
+sgMail.setApiKey(API_KEY);
+const msg = {
+  to: postulacion.email,
+  from: "enviocorreomunicipalidad@gmail.com",
+  subject: "Respuesta de postulación",
+  text: "Comunicado", 
+  html: "<strong>Hola, su postulacion ha sido aprobada. </strong>",
+};
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Correo enviado');
+  })
+  .catch((error) => {
+    console.error('Error al enviar el correo:', error);
+  });
+    }else{
+      const API_KEY = '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';
+sgMail.setApiKey(API_KEY);
+const msg = {
+  to: postulacion.email,
+  from: "enviocorreomunicipalidad@gmail.com",
+  subject: "Respuesta de postulación",
+  text: "Comunicado", 
+  html: "<strong>Hola, su postulacion ha sido rechazada debido a que no cumple con todos los requisitos. </strong>",
+};
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Correo enviado');
+  })
+  .catch((error) => {
+    console.error('Error al enviar el correo:', error);
+  });
+    }
+
     const respuestaSave = await respuesta.save();
     res.status(201).json(respuestaSave);
 
@@ -45,6 +86,58 @@ export const clasificarPostulacion = async (req, res) => {
     res.status(500).json({ message: 'Error al clasificar la postulación, verifique que la id introducida es la correcta' });
   }
 };
+
+/*export const clasificarPostulacion = async (req, res) => {
+  try {
+    const { evaluadorId, postulacionId, evaluar } = req.body;
+
+    const postulacion = await Postulacion.findById(postulacionId);
+
+    if (postulacion.evaluador) {
+      console.log('Esta postulación ya fue asignada a un evaluador');
+      return res.status(400).json({ message: 'Esta postulación ya fue asignada a un evaluador' });
+    }
+
+    const evaluador = await Evaluador.findById(evaluadorId);
+
+    const evaluadorAsignado = await Respuesta.findOne({ postulacion: postulacionId });
+    if (evaluadorAsignado) {
+      console.log('Esta postulación ya fue evaluada por otro evaluador');
+      return res.status(400).json({ message: 'Esta postulación ya fue evaluada por otro evaluador' });
+    }
+
+    if (evaluar !== 'aprobado' && evaluar !== 'rechazado') {
+      console.log('El parámetro "evaluar" debe ser "aprobado" o "rechazado"');
+      return res.status(400).json({ message: 'El parámetro evaluar debe ser aprobado o rechazado' });
+    }
+
+    const respuesta = new Respuesta({
+      postulacion: postulacionId,
+      evaluador: evaluadorId,
+      evaluar: evaluar,
+    });
+
+    // Obtener el email de la postulación
+    const email = postulacion.email;
+
+    // Configurar el envío de correo
+   
+
+    const respuestaSave = await respuesta.save();
+    await sgMail.send(msg);
+
+    res.status(201).json(respuestaSave);
+
+    postulacion.evaluador = evaluadorId;
+    await postulacion.save();
+    await evaluador.save();
+
+    console.log('La postulación ha sido evaluada y clasificada exitosamente');
+  } catch (error) {
+    console.error('Error al clasificar la postulación:', error);
+    res.status(500).json({ message: 'Error al clasificar la postulación, verifique que la id introducida es la correcta' });
+  }
+};*/
 
 
 export const getRespuestas = async (req, res) => {
