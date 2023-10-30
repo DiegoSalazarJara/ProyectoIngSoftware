a291ffbf1ab132f5105754723840e031b0d3505b
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2023-10-30 01:44:46 -0300

    modificacion schema postulacion

--- src/schema/postulacion.schema.js
+++ src/schema/postulacion.schema.js
@@ -21,12 +21,6 @@ export const formBodySchema = Joi.object({
     "string.min": "El rut del postulante debe tener al menos 10 caracteres.",
     "string.pattern.base": "El rut del postulante tiene el formato XXXXXXXX-X, ejemplo: 12345678-9.",
   }),
-  email: Joi.string().label('email').email().required().messages({
-    "string.empty": "El email no puede estar vacío.",
-    "any.required": "El email es obligatorio.",
-    "string.base": "El email debe ser de tipo string.",
-    "string.email": "El email debe tener un formato válido.",
-  }),
   email: Joi.string().label('email').required().email().pattern(new RegExp("^[A-Za-z0-9]+@[A-Za-z0-9]+\\.[A-Za-z]+$"))
     .messages({
       "string.empty": "El email no puede estar vacío.",
@@ -41,12 +35,12 @@ export const formBodySchema = Joi.object({
     "string.base": "El nombre de la empresa debe ser de tipo string.",
     "string.pattern.base": "El nombre de la empresa solo puede contener letras del alfabeto.",
   }),
- rutempresa: Joi.string().label('rutempresa').required().min(10).pattern(/^[0-9]+-[0-9kK]{1}$/).message({
+ rutempresa: Joi.string().label('rutempresa').required().min(10).pattern(/^3[0-9]+-[0-9kK]{1}$/).message({
     "string.empty": "El rut de la empresa no puede estar vacío.",
     "any.required": "El rut de la empresa es obligatorio.",
     "string.base": "El rut de la empresa debe ser de tipo string.",
     "string.min": "El rut de la empresa debe tener al menos 10 caracteres.",
-    "string.pattern.base": "El rut de la empresa tiene el formato XXXXXXXX-X, ejemplo: 12345678-9.",
+    "string.pattern.base": "El rut de la empresa tiene el formato XXXXXXXX-X, ejemplo: 32345678-9.",
   }),
   direccionEmpresa: Joi.string().label('direccionEmpresa').required().messages({
     "string.empty": "La direccion de la empresa no puede estar vacía.",
@@ -58,7 +52,7 @@ export const formBodySchema = Joi.object({
       'string.empty': 'El campo tipoPatente no puede estar vacío.',
       'string.base': 'El campo tipoPatente debe ser de tipo string.',
       'any.only': 'El valor del campo tipoPatente no es válido.',
-      "string.pattern.base": "El tipo de patente solo puede contener letras del alfabeto.",
+      "string.pattern.base": "El tipoPatente solo puede contener letras del alfabeto.",
     }),
     certificadoResidencia: Joi.string().label('certificadoResidencia')
     .required()
@@ -105,12 +99,6 @@ export const formUpdateBodySchema = Joi.object({
     "string.base": "El rut del postulante debe ser de tipo string.",
     "string.min": "El rut del postulante debe tener al menos 10 caracteres.",
     "string.pattern.base": "El rut del postulante tiene el formato XXXXXXXX-X, ejemplo: 12345678-9.",
-  }),
-  email: Joi.string().label('email').email().required().messages({
-    "string.empty": "El email no puede estar vacío.",
-    "any.required": "El email es obligatorio.",
-    "string.base": "El email debe ser de tipo string.",
-    "string.email": "El email debe tener un formato válido.",
   }),
   email: Joi.string().label('email').required().email().pattern(new RegExp("^[A-Za-z0-9]+@[A-Za-z0-9]+\\.[A-Za-z]+$"))
     .messages({
