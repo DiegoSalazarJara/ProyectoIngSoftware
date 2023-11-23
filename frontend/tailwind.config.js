851169938b6b3879773a8e0e5eb4bfc4b6c715a1
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2023-11-23 02:24:41 -0300

    Implementacion tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        blanco:'#ECF0F1',
+        azul:'#3498DB',
+        rojo:'#E74C3C',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
+
