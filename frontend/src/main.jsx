a0f695efb7674f480533183598fc2c4bbbf78551
magdielaraneda <aniiko.magdi@gmail.com>
2023-12-14 00:31:08 -0300

    Bar y Main con pagare y evaluador agregados

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,9 @@ import Root from './routes/Root.jsx';
 import ErrorPage from './routes/ErrorPage.jsx';
 import Login from './routes/Login.jsx';
 import Formulario from './routes/formPostulacion.jsx';
+import Patente from './routes/Patente.jsx';
+import EmitirPagare from './routes/EmitirPagare.jsx';
+import CrearEvaluador from './routes/CrearEvaluador.jsx';
 
 const router = createBrowserRouter([
   {
@@ -16,15 +19,27 @@ const router = createBrowserRouter([
         path: '/',
         element: <App />,
       },
+      {
+        path: '/postulacion',
+        element: <Formulario/>,
+      },
+      {
+        path: '/patente',
+        element: <Patente/>,
+      },
+      {
+        path: '/pagare',
+        element: <EmitirPagare/>,
+      },
+      {
+        path: '/CrearEvaluador',
+        element: <CrearEvaluador/>,
+      },
     ],
   },
   {
     path: '/auth',
     element: <Login />,
-  },
-  {
-    path: '/postulacion',
-    element: <Formulario/>
   }
 ]);
 
