cb3cc41b3694d583cc76de40cc45ca81d574ec91
magdielaraneda <aniiko.magdi@gmail.com>
2023-12-14 00:25:15 -0300

    Parte Frontend Pagare

---
+++ frontend/src/components/FormPagare.jsx
@@ -0,0 +1,74 @@
+import 'tailwindcss/tailwind.css';
+import { useForm } from "react-hook-form";
+
+export default function FormPagare() {
+
+  const {
+    register,
+    formState: { errors },
+    watch,
+    setValue,
+    reset,
+  } = useForm({
+    defaultValues: {
+      idPostulante: "",
+      idEvaluador: "",
+    },
+  });
+
+  return (
+    <div>
+    <div className="mt-10">
+    <form className="mx-auto max-w-sm">
+      <div className="space-y-4">
+        <div>
+        <h1 className="text-base font-semibold leading-9 text-gray-900 text-center">Formulario Emisión Pagare</h1>
+        </div>
+
+        <div className="border-b border-gray-900/10 pb-8">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
+            <div className="col-span-full">
+              <label htmlFor="idpostulante" className="block text-sm font-medium leading-6 text-gray-900">
+                Id Postulante
+              </label>
+              <div className="mt-2">
+              <input
+              type="text"
+              name="idpostulante"
+              id="idpostulante"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+              
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label htmlFor="idevaluador" className="block text-sm font-medium leading-6 text-gray-900">
+                id Evaluador
+              </label>
+              <div className="mt-2">
+              <input
+              type="text" 
+              name="idevaluador"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>  
+      <div className="mt-6 flex items-center justify-center gap-x-6">
+        <button
+          type="submit"
+          className="rounded-md bg-azul px-8 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Enviar
+        </button>
+      </div>
+
+    </form>
+    </div>
+    </div>
+  )
+}
---
+++ frontend/src/routes/EmitirPagare.jsx
@@ -0,0 +1,17 @@
+import FormPagare from '../components/Formpagare.jsx'
+
+export default function EmitirPagare() {
+  
+  return (
+    <>
+    <div>
+    <header className="bg-white shadow">
+          <div className=" max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Emitir Pagare</h1>
+          </div>
+      </header>
+    </div>
+    <FormPagare/>
+    </>
+  );
+}
