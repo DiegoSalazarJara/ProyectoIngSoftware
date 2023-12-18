afc9cfac69ae156c1c5b6a3a54d508c0b092047c
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2023-12-18 05:07:11 -0300

    fix evaluadores

--- frontend/src/routes/VerEvaluadores.jsx
+++ frontend/src/routes/VerEvaluadores.jsx
@@ -4,14 +4,7 @@ export default function ListarEvaluadores() {
 
     return (
       <>
-      <div>
-      <header className="bg-white shadow">
-            <div className=" max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold tracking-tight text-gray-900">Evaluadores</h1>
-            </div>
-        </header>
-      </div>
-      <ListarEvaluadores/>
+      <VerEvaluadores/>
       </>
     );
   }
