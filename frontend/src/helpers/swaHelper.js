bf65df11816805a084a58293782061c870f1ec8a
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2023-12-17 21:52:11 -0300

    modificacion alertas

--- frontend/src/helpers/swaHelper.js
+++ frontend/src/helpers/swaHelper.js
@@ -33,44 +33,38 @@ export const showNotFoundForm = async () => {
   });
 }
 
-  export const showDeleteForm = async () => {
+export const showDeleteForm = async () => {
+  await Toast.fire({
+    icon: "success",
+    title: "Postulación eliminada correctamente"
+  });
+}
+
+export const showRenovar = async (response) => {
+  await Toast.fire({
+    icon: "info",
+    title: response
+  });
+}
+
+export const showCancelar = async () => {
+  await Toast.fire({
+    icon: "success",
+    title: "Patente cancelada correctamente"
+  });
+}
+
+export const showConfirmPatente = async () => {
+  await Toast.fire({
+    icon: "success",
+    title: "Patente creada exitosamente!"
+  });
+};
+
+export const showErrorPatente = async () => {
     await Toast.fire({
-      icon: "success",
-      title: "Postulación eliminada correctamente"
+      icon: "error",
+      title: "Error al crear la patente"
     });
 }
 
-/*
-export const { value: fruit } = await Swal.fire({
-  title: "Select field validation",
-  input: "select",
-  inputOptions: {
-    Fruits: {
-      apples: "Apples",
-      bananas: "Bananas",
-      grapes: "Grapes",
-      oranges: "Oranges"
-    },
-    Vegetables: {
-      potato: "Potato",
-      broccoli: "Broccoli",
-      carrot: "Carrot"
-    },
-    icecream: "Ice cream"
-  },
-  inputPlaceholder: "Select a fruit",
-  showCancelButton: true,
-  inputValidator: (value) => {
-    return new Promise((resolve) => {
-      if (value === "oranges") {
-        resolve();
-      } else {
-        resolve("You need to select oranges :)");
-      }
-    });
-  }
-});
-if (fruit) {
-  Swal.fire(`You selected: ${fruit}`);
-}
-*/
