2df188af924174e4361578b122f3db83c081e5cb
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2023-12-18 03:49:47 -0300

    modificacion login

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -83,7 +83,7 @@ import { useForm } from 'react-hook-form';
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 type="submit"
               >
-                Sign in
+                Iniciar Sesión
               </button>
             </div>
           </form>
